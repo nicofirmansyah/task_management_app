2e98442d4e1abee4a678e6217abc350b18e8b830
nicofirmansyah <nicofirmansyah057@gmail.com>
2022-12-03 18:15:51 +0700

    Search Data From Firestore Database 01

--- lib/app/modules/friends/views/friends_view.dart
+++ lib/app/modules/friends/views/friends_view.dart
@@ -1,8 +1,8 @@
-
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/app/data/controller/auth_controller.dart';
 import 'package:task_management_app/app/utils/style/AppColors.dart';
 import 'package:task_management_app/app/utils/widget/header.dart';
 import 'package:task_management_app/app/utils/widget/myFriends.dart';
@@ -12,73 +12,110 @@ import '../controllers/friends_controller.dart';
 
 class FriendsView extends GetView<FriendsController> {
   final GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
-
+  final authCon = Get.find<AuthController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _drawerKey,
       drawer: const SizedBox(width: 150, child: SideBar()),
       backgroundColor: AppColors.primaryBg,
-      body:SafeArea(
+      body: SafeArea(
         child: Row(
-        children: [
-          !context.isPhone
-           ? const Expanded(
-            flex: 2,
-            child:   SideBar()
-            )
-            :const SizedBox(),
-          Expanded(
-            flex: 15,
-            child: Column(children: [
-           !context.isPhone
-            ? const header()
-            : Container(padding: EdgeInsets.all(20),
-           child: Row(children: [IconButton(onPressed: (){
-            _drawerKey.currentState!.openDrawer();
-           },
-            // ignore: prefer_const_constructors
-            icon: Icon(
-              Icons.menu,
-              color:AppColors.primaryText,
-           ),
-           ),
-           const SizedBox(
-            width: 15,
-             ),
-           Column(
-           // mainAxisAlignment: MainAxisAlignment.start,
-           crossAxisAlignment: CrossAxisAlignment.start,
-          children:const [
-            Text(
-              'Task Management' ,
-              style: TextStyle(fontSize: 20, color: AppColors.primaryText),
+          children: [
+            !context.isPhone
+                ? const Expanded(flex: 2, child: SideBar())
+                : const SizedBox(),
+            Expanded(
+              flex: 15,
+              child: Column(
+                children: [
+                  !context.isPhone
+                      ? const header()
+                      : Container(
+                          padding: EdgeInsets.all(20),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  IconButton(
+                                    onPressed: () {
+                                      _drawerKey.currentState!.openDrawer();
+                                    },
+                                    // ignore: prefer_const_constructors
+                                    icon: Icon(
+                                      Icons.menu,
+                                      color: AppColors.primaryText,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 15,
+                                  ),
+                                  Column(
+                                    // mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: const [
+                                      Text(
+                                        'Task Management',
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            color: AppColors.primaryText),
+                                      ),
+                                      Text(
+                                        'Manage Task Made Easy',
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            color: AppColors.primaryText),
+                                      ),
+                                    ],
+                                  ),
+                                  const Spacer(),
+                                  const Icon(Icons.notifications,
+                                      color: AppColors.primaryText, size: 25),
+                                  const SizedBox(
+                                    width: 15,
+                                  ),
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(30),
+                                    child: const CircleAvatar(
+                                      backgroundColor: Colors.amber,
+                                      radius: 25,
+                                      foregroundImage: NetworkImage(
+                                          'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80'),
+                                    ),
+                                  )
+                                ],
+                              ),
+                         const SizedBox(height: 10,),
+                         context.isPhone
+                         ? TextField(
+                          onChanged: (value) => authCon.searchFriend(value),
+                          controller: authCon.searchFriendsController,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: 
+              const EdgeInsets.only(left: 40, right: 30),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: const BorderSide(color: Colors.white),
               ),
-            Text(
-              'Manage Task Made Easy',
-               style: TextStyle(fontSize: 12, color: AppColors.primaryText),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: const BorderSide(color: Colors.blue),
               ),
-          ],
-          ),
-          const Spacer(),
-          const Icon(Icons.notifications, 
-          color: AppColors.primaryText, size: 25
-          ),
-          const SizedBox(
-            width: 15,
-             ),
-          ClipRRect(borderRadius: BorderRadius.circular(30),
-          child: const CircleAvatar(
-            backgroundColor: Colors.amber,
-            radius: 25,
-            foregroundImage: NetworkImage('https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80'),
-          ),
-          )
-           ],
-           ),
-           ),
-            //CONTENT /ISI PAGE / SCREEN
-              Expanded(
+              prefixIcon: const Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              hintText: 'Search',
+            ),
+          ):const SizedBox(),
+                            ],
+                            
+                          ),
+                        ),
+                  //CONTENT /ISI PAGE / SCREEN
+                  Expanded(
                     child: Container(
                       padding: !context.isPhone
                           ? const EdgeInsets.all(50)
@@ -92,7 +129,8 @@ class FriendsView extends GetView<FriendsController> {
                             ? BorderRadius.circular(50)
                             : BorderRadius.circular(30),
                       ),
-                      child: Column(
+                      child: Obx(() => authCon.hasilPencarian.isEmpty
+                      ? Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             const Text(
@@ -113,18 +151,20 @@ class FriendsView extends GetView<FriendsController> {
                                     child: Stack(
                                       children: [
                                         ClipRRect(
-                                          borderRadius: BorderRadius.circular(50),
+                                          borderRadius:
+                                              BorderRadius.circular(50),
                                           child: const Image(
                                             image: NetworkImage(
                                                 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80'),
                                           ),
                                         ),
-                                        const Positioned( 
+                                        const Positioned(
                                           bottom: 10,
                                           left: 55,
                                           child: Text(
                                             "Nico Firmansyah",
-                                            style: TextStyle(color: Colors.white),
+                                            style:
+                                                TextStyle(color: Colors.white),
                                           ),
                                         ),
                                         Positioned(
@@ -142,8 +182,9 @@ class FriendsView extends GetView<FriendsController> {
                                                       BorderRadius.circular(50),
                                                 ),
                                               ),
-                                              child:
-                                                  Icon(Icons.add_circle_outline),
+                                              // ignore: prefer_const_constructors
+                                              child: Icon(
+                                                  Icons.add_circle_outline),
                                             ),
                                           ),
                                         ),
@@ -153,10 +194,27 @@ class FriendsView extends GetView<FriendsController> {
                                 },
                               ),
                             ),
-                            MyFriends(),
-                          ]),
+                            const MyFriends(),
+                          ])
+                          :ListView.builder(
+                            padding: const EdgeInsets.all(8),
+                            shrinkWrap: true,
+                            itemCount: authCon.hasilPencarian.length,
+                            itemBuilder: (context, index) =>ListTile(leading:ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(50),
+                                          child:  Image(
+                                            image: NetworkImage(authCon.hasilPencarian[index] ['photo']),
+                                          ),
+                                        ),
+                                        title: Text(authCon.hasilPencarian[index] ['name']),
+                                        subtitle: Text(authCon.hasilPencarian[index] ['email']),
+                                        trailing: Icon(Icons.add),
+                                         ) ,
+                          ),
+                          ),
+                          ),
                     ),
-                  )
                 ],
               ),
             ),
